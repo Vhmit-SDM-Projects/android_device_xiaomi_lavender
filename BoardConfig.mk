d7d701a12571307abd83bc96e41752fbf22516c2
Vhmit <viktor.9630@hotmail.com>
2023-10-19 22:24:10 -0300

    lavender: Clean-up device tree
    * Move configurations to configs
    * Move hals to hidl
    
    Change-Id: Ib9494410f19888d6f469b00f66fab5410a15b6c3
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,9 +79,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 3.0
 
 # HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lavender
