b6598742ae43b438e1db15687d3f2d58b24c6010
Vhmit <viktor.9630@hotmail.com>
2023-10-24 08:55:53 -0300

    lavender: Switch to Camera Aperture
    * Replace AOSP Camera
    
    Change-Id: I70eb2ae035d039a5cc8d91a71dd13fbc8e816e03
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- device.mk
+++ device.mk
@@ -87,6 +87,9 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
+PRODUCT_PACKAGES += \
+    Aperture
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
